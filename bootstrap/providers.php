fb206baf120a98622f607a9ea88e81b14384d445
Tomáš Douděra <tomasdoudera@gmail.com>
2026-03-14 21:37:27 +0100

    [Fix]

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,9 +3,11 @@
 use App\Providers\AppServiceProvider;
 use App\Providers\Filament\AdminPanelProvider;
 use App\Providers\FortifyServiceProvider;
+use TomasDoudera\CalMe\CalMeServiceProvider;
 
 return [
     AppServiceProvider::class,
     AdminPanelProvider::class,
     FortifyServiceProvider::class,
+    CalMeServiceProvider::class,
 ];
